5ce518d9f2b9835db63ee8b48e9c13dfd1d64ff1
Rorical <hash_548@163.com>
2021-04-30 11:43:27 +0800

    structor generator?

---
+++ gostructor.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+
+import json
+
+jsonStr = '{"error":false,"message":"","body":{"illustId":"89125708","illustTitle":"\u843d\u66f84.13","illustComment":"\u273f\u273f\u30fd(\u00b0\u25bd\u00b0)\u30ce\u273f","id":"89125708","title":"\u843d\u66f84.13","description":"\u273f\u273f\u30fd(\u00b0\u25bd\u00b0)\u30ce\u273f","illustType":0,"createDate":"2021-04-13T06:56:38+00:00","uploadDate":"2021-04-13T06:56:38+00:00","restrict":0,"xRestrict":0,"sl":2,"urls":{"mini":"https:\/\/i.pximg.net\/c\/48x48\/img-master\/img\/2021\/04\/13\/15\/56\/38\/89125708_p0_square1200.jpg","thumb":"https:\/\/i.pximg.net\/c\/250x250_80_a2\/img-master\/img\/2021\/04\/13\/15\/56\/38\/89125708_p0_square1200.jpg","small":"https:\/\/i.pximg.net\/c\/540x540_70\/img-master\/img\/2021\/04\/13\/15\/56\/38\/89125708_p0_master1200.jpg","regular":"https:\/\/i.pximg.net\/img-master\/img\/2021\/04\/13\/15\/56\/38\/89125708_p0_master1200.jpg","original":"https:\/\/i.pximg.net\/img-original\/img\/2021\/04\/13\/15\/56\/38\/89125708_p0.jpg"},"tags":{"authorId":"89861","isLocked":false,"tags":[{"tag":"\u30e0\u30e9\u30b5\u30e1(\u5343\u604b*\u4e07\u82b1)","locked":true,"deletable":false,"userId":"89861","translation":{"en":"\u4e1b\u96e8(\u5343\u604b\u4e07\u82b1)"},"userName":"\u5c0f\u5361"},{"tag":"\u5343\u604b\u4e07\u82b1","locked":true,"deletable":false,"userId":"89861","translation":{"en":"Senren*Banka"},"userName":"\u5c0f\u5361"},{"tag":"\u30e0\u30e9\u30b5\u30e1","locked":true,"deletable":false,"userId":"89861","translation":{"en":"\u6751\u96e8"},"userName":"\u5c0f\u5361"},{"tag":"\u5343\u604b*\u4e07\u82b1","locked":true,"deletable":false,"userId":"89861","translation":{"en":"\u5343\u604b\u4e07\u82b1"},"userName":"\u5c0f\u5361"},{"tag":"\u3086\u305a\u30bd\u30d5\u30c8","locked":true,"deletable":false,"userId":"89861","translation":{"en":"\u67da\u5b50\u793e"},"userName":"\u5c0f\u5361"}],"writable":true},"alt":"#\u30e0\u30e9\u30b5\u30e1(\u5343\u604b*\u4e07\u82b1) \u843d\u66f84.13 - \u5c0f\u5361\u7684\u63d2\u753b","storableTags":["dkEx38uZvm","G3tpC3jXa-","Gbk7a-U24b","MTb6elxam3","Kh4mg2DS4s"],"userId":"89861","userName":"\u5c0f\u5361","userAccount":"cariboy","userIllusts":{"89125708":{"id":"89125708","title":"\u843d\u66f84.13","illustType":0,"xRestrict":0,"restrict":0,"sl":2,"url":"https:\/\/i.pximg.net\/c\/250x250_80_a2\/img-master\/img\/2021\/04\/13\/15\/56\/38\/89125708_p0_square1200.jpg","description":"\u273f\u273f\u30fd(\u00b0\u25bd\u00b0)\u30ce\u273f","tags":["\u30e0\u30e9\u30b5\u30e1(\u5343\u604b*\u4e07\u82b1)","\u5343\u604b\u4e07\u82b1","\u30e0\u30e9\u30b5\u30e1","\u5343\u604b*\u4e07\u82b1","\u3086\u305a\u30bd\u30d5\u30c8"],"userId":"89861","userName":"\u5c0f\u5361","width":1164,"height":1200,"pageCount":1,"isBookmarkable":true,"bookmarkData":null,"alt":"#\u30e0\u30e9\u30b5\u30e1(\u5343\u604b*\u4e07\u82b1) \u843d\u66f84.13 - \u5c0f\u5361\u7684\u63d2\u753b","titleCaptionTranslation":{"workTitle":"","workCaption":""},"createDate":"2021-04-13T15:56:38+09:00","updateDate":"2021-04-13T15:56:38+09:00","isUnlisted":false,"isMasked":false},"88372007":{"id":"88372007","title":"\u843d\u66f83.11","illustType":0,"xRestrict":0,"restrict":0,"sl":2,"url":"https:\/\/i.pximg.net\/c\/250x250_80_a2\/img-master\/img\/2021\/03\/11\/18\/02\/18\/88372007_p0_square1200.jpg","description":"","tags":["\u9b54\u5973\u306e\u65c5\u3005","\u30a4\u30ec\u30a4\u30ca","summerpockets","\u795e\u5c71\u8b58"],"userId":"89861","userName":"\u5c0f\u5361","width":1170,"height":1170,"pageCount":2,"isBookmarkable":true,"bookmarkData":null,"alt":"#\u9b54\u5973\u306e\u65c5\u3005 \u843d\u66f83.11 - \u5c0f\u5361\u7684\u63d2\u753b","titleCaptionTranslation":{"workTitle":"","workCaption":""},"createDate":"2021-03-11T18:02:18+09:00","updateDate":"2021-03-11T18:02:18+09:00","isUnlisted":false,"isMasked":false,"profileImageUrl":"https:\/\/i.pximg.net\/user-profile\/img\/2011\/05\/14\/23\/59\/47\/3125348_f6d26e8b2391a7cb2a44bf396a9739ba_50.jpg"},"87344915":null,"86552103":null,"85578826":null,"85492210":null,"84665888":null,"84665842":null,"81490736":null,"80502339":null,"79909255":null,"79793887":null,"79749855":null,"79180683":null,"78622141":null,"78009456":null,"77970951":null,"77921513":null,"77879039":null,"77727497":null,"77696800":null,"77569511":null,"77426268":null,"77231798":null,"77216942":null,"76651620":null,"76645181":null,"76532272":null,"76348201":null,"76087238":null,"76032619":null,"75733719":null,"74921460":null,"74553719":null,"74387013":null,"73726667":null,"73633485":null,"73487503":null,"73187064":null,"73122457":null,"72645957":null,"72601604":null,"72522686":null,"72481387":null,"72455259":null,"72269579":null,"72210581":null,"72194193":null,"71989455":null,"71833055":null,"71782684":null,"71622613":null,"71531631":null,"71476107":null,"71434083":null,"71306324":null,"71209219":null,"71157453":null,"70847677":null,"70826824":null,"70644304":null,"70469561":null,"70440995":null,"70426699":null,"70315635":null,"70208481":null,"70171193":null,"70088526":null,"69666468":null,"69224856":null,"69088998":null,"68745883":null,"67857235":null,"66956431":null,"66912942":null,"66450775":null,"66352334":null,"66352295":null,"66207209":null,"65892644":null,"65859375":null,"65778669":null,"65701396":null,"65627395":null,"65413746":null,"65142202":null,"64938903":null,"64936452":null,"64152613":null,"63865077":null,"63845095":null,"63842713":null,"63842669":null,"62898137":null,"62558465":null,"62269065":null,"62121527":null,"61891438":null,"61779068":null,"61722233":null,"59959150":null,"54311056":null,"54146699":null,"53376065":null,"53271712":null,"53142453":null,"46572596":null,"39750054":null,"35671572":null,"35081087":null,"31347435":null,"23322524":null,"18882071":null,"17646967":null,"17513394":null,"6430115":null,"4948507":null,"3672447":null,"3671967":null,"3291654":null,"3204992":null,"2393248":null,"2321247":null},"likeData":false,"width":1164,"height":1200,"pageCount":1,"bookmarkCount":1201,"likeCount":842,"commentCount":32,"responseCount":0,"viewCount":3758,"isHowto":false,"isOriginal":false,"imageResponseOutData":[],"imageResponseData":[],"imageResponseCount":0,"pollData":null,"seriesNavData":null,"descriptionBoothId":null,"descriptionYoutubeId":null,"comicPromotion":null,"fanboxPromotion":null,"contestBanners":[],"isBookmarkable":true,"bookmarkData":null,"contestData":null,"zoneConfig":{"responsive":{"url":"https:\/\/pixon.ads-pixiv.net\/show?zone_id=illust_responsive_side\u0026format=js\u0026s=1\u0026up=0\u0026a=30\u0026ng=w\u0026l=zh\u0026uri=%2Fajax%2Fillust%2F_PARAM_\u0026is_spa=1\u0026K=51fcea11ef5348\u0026ab_test_digits_first=66\u0026uab=97\u0026yuid=IFdzNQY\u0026suid=Pgpx7ksuqw3bovpzm\u0026num=60867497671"},"rectangle":{"url":"https:\/\/pixon.ads-pixiv.net\/show?zone_id=illust_rectangle\u0026format=js\u0026s=1\u0026up=0\u0026a=30\u0026ng=w\u0026l=zh\u0026uri=%2Fajax%2Fillust%2F_PARAM_\u0026is_spa=1\u0026K=51fcea11ef5348\u0026ab_test_digits_first=66\u0026uab=97\u0026yuid=IFdzNQY\u0026suid=Pgpx7ksuqzsoboskk\u0026num=60867497600"},"500x500":{"url":"https:\/\/pixon.ads-pixiv.net\/show?zone_id=bigbanner\u0026format=js\u0026s=1\u0026up=0\u0026a=30\u0026ng=w\u0026l=zh\u0026uri=%2Fajax%2Fillust%2F_PARAM_\u0026is_spa=1\u0026K=51fcea11ef5348\u0026ab_test_digits_first=66\u0026uab=97\u0026yuid=IFdzNQY\u0026suid=Pgpx7ksur2wqxms3\u0026num=60867497209"},"header":{"url":"https:\/\/pixon.ads-pixiv.net\/show?zone_id=header\u0026format=js\u0026s=1\u0026up=0\u0026a=30\u0026ng=w\u0026l=zh\u0026uri=%2Fajax%2Fillust%2F_PARAM_\u0026is_spa=1\u0026K=51fcea11ef5348\u0026ab_test_digits_first=66\u0026uab=97\u0026yuid=IFdzNQY\u0026suid=Pgpx7ksur5ae0n5mk\u0026num=60867497353"},"footer":{"url":"https:\/\/pixon.ads-pixiv.net\/show?zone_id=footer\u0026format=js\u0026s=1\u0026up=0\u0026a=30\u0026ng=w\u0026l=zh\u0026uri=%2Fajax%2Fillust%2F_PARAM_\u0026is_spa=1\u0026K=51fcea11ef5348\u0026ab_test_digits_first=66\u0026uab=97\u0026yuid=IFdzNQY\u0026suid=Pgpx7ksur7lin57ok\u0026num=608674975"},"expandedFooter":{"url":"https:\/\/pixon.ads-pixiv.net\/show?zone_id=multiple_illust_viewer\u0026format=js\u0026s=1\u0026up=0\u0026a=30\u0026ng=w\u0026l=zh\u0026uri=%2Fajax%2Fillust%2F_PARAM_\u0026is_spa=1\u0026K=51fcea11ef5348\u0026ab_test_digits_first=66\u0026uab=97\u0026yuid=IFdzNQY\u0026suid=Pgpx7ksuraiycrrqh\u0026num=60867497532"},"logo":{"url":"https:\/\/pixon.ads-pixiv.net\/show?zone_id=logo_side\u0026format=js\u0026s=1\u0026up=0\u0026a=30\u0026ng=w\u0026l=zh\u0026uri=%2Fajax%2Fillust%2F_PARAM_\u0026is_spa=1\u0026K=51fcea11ef5348\u0026ab_test_digits_first=66\u0026uab=97\u0026yuid=IFdzNQY\u0026suid=Pgpx7ksurd0drg1mg\u0026num=60867497485"},"relatedworks":{"url":"https:\/\/pixon.ads-pixiv.net\/show?zone_id=relatedworks\u0026format=js\u0026s=1\u0026up=0\u0026a=30\u0026ng=w\u0026l=zh\u0026uri=%2Fajax%2Fillust%2F_PARAM_\u0026is_spa=1\u0026K=51fcea11ef5348\u0026ab_test_digits_first=66\u0026uab=97\u0026yuid=IFdzNQY\u0026suid=Pgpx7ksurgqj1dxnu\u0026num=60867497353"}},"extraData":{"meta":{"title":"#\u30e0\u30e9\u30b5\u30e1(\u5343\u604b*\u4e07\u82b1) \u843d\u66f84.13 - \u5c0f\u5361\u7684\u63d2\u753b - pixiv","description":"\u3053\u306e\u4f5c\u54c1 \u300c\u843d\u66f84.13\u300d \u306f \u300c\u30e0\u30e9\u30b5\u30e1(\u5343\u604b*\u4e07\u82b1)\u300d\u300c\u5343\u604b\u4e07\u82b1\u300d \u7b49\u306e\u30bf\u30b0\u304c\u3064\u3051\u3089\u308c\u305f\u300c\u5c0f\u5361\u300d\u3055\u3093\u306e\u30a4\u30e9\u30b9\u30c8\u3067\u3059\u3002 \u300c\u273f\u273f\u30fd(\u00b0\u25bd\u00b0)\u30ce\u273f\u300d","canonical":"https:\/\/www.pixiv.net\/artworks\/89125708","alternateLanguages":{"ja":"https:\/\/www.pixiv.net\/artworks\/89125708","en":"https:\/\/www.pixiv.net\/en\/artworks\/89125708"},"descriptionHeader":"\u672c\u4f5c\u300c\u843d\u66f84.13\u300d\u4e3a\u9644\u6709\u300c\u30e0\u30e9\u30b5\u30e1(\u5343\u604b*\u4e07\u82b1)\u300d\u300c\u5343\u604b\u4e07\u82b1\u300d\u7b49\u6807\u7b7e\u7684\u63d2\u753b\u3002","ogp":{"description":"\u273f\u273f\u30fd(\u00b0\u25bd\u00b0)\u30ce\u273f","image":"https:\/\/embed.pixiv.net\/decorate.php?illust_id=89125708","title":"#\u30e0\u30e9\u30b5\u30e1(\u5343\u604b*\u4e07\u82b1) \u843d\u66f84.13 - \u5c0f\u5361\u7684\u63d2\u753b - pixiv","type":"article"},"twitter":{"description":"\u273f\u273f\u30fd(\u00b0\u25bd\u00b0)\u30ce\u273f","image":"https:\/\/embed.pixiv.net\/decorate.php?illust_id=89125708","title":"\u843d\u66f84.13","card":"summary_large_image"}}},"titleCaptionTranslation":{"workTitle":"","workCaption":""},"isUnlisted":false,"request":null}}'
+
+
+class Struct():
+    class Field():
+        def __init__(self, name, types, tag):
+            if name[0].isdigit():
+                name = "_" + name
+            self.name = name
+            self.type = types
+            self.tag = tag
+        def toStr(self):
+            return '%s %s `json:"%s"`' % (self.name, self.type, self.tag)
+    def __init__(self, name):
+        self.fields = []
+        self.name = name
+    def addField(self, name, types, original):
+        self.fields.append(self.Field(name, types, original))
+    def toStr(self):
+        result = "type %s struct {" % self.name
+        for key, field in enumerate(self.fields):
+            result += field.toStr()
+            if key < len(self.fields) - 1:
+                result += "\n"
+        result += "}"
+        return result
+
+class Structor():
+    def __init__(self, name):
+        self.name = name
+        self.structList = []
+        self.typeList = {int: "int", str: "string", bool: "bool", type(None): "interface{}"}
+    def clear(self):
+        self.structList.clear()
+    def load(self, jsonStr):
+        jsonObj = json.loads(jsonStr)
+        self.generate(jsonObj)
+    def generate(self, jsonObj, name = ""):
+        struct = Struct((self.name if name == "" else self.name + name))
+        for key, value in jsonObj.items():
+            types = type(value)
+            if types == dict:
+                self.generate(value, key.capitalize())
+                struct.addField(key.capitalize(), "[]%s" % (self.name + key.capitalize()), key)
+            elif types == list:
+                oneType = self.isListAllOneType(value)
+                if oneType:
+                    if oneType in self.typeList:
+                        oneType = self.typeList[oneType]
+                        struct.addField(key.capitalize(), "[]%s" % oneType, key)
+                    else:
+                        self.generate(value[0], key.capitalize())
+                        struct.addField(key.capitalize(), "[]%s" % (self.name + key.capitalize()), key)
+                else:
+                    struct.addField(key.capitalize(), "[]interface{}", key)
+            else:
+                struct.addField(key.capitalize(), self.typeList[types], key)
+        self.structList.append(struct)
+    def isListAllOneType(self, theList):
+        if len(theList) == 0:
+            return False
+        theType = type(theList[0])
+        for value in theList:
+            if type(value) != theType:
+                return False
+        return theType
+    def returnCode(self):
+        result = ""
+        for key, struct in enumerate(self.structList):
+            result += struct.toStr()
+            if key < len(self.structList) - 1:
+                result += "\n"
+        return result
+        
+structor = Structor("PixivResponse")
+structor.load(jsonStr)
+print(structor.returnCode())
